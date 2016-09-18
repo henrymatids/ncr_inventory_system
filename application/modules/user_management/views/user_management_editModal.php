807a46ec529576a7500b71aecc061590265a2cbe
Henry Senerpida Matidios <henrymatids@gmail.com>
2016-09-19 01:47:16 +0800

    added user management

---
+++ application/modules/user_management/views/user_management_editModal.php
@@ -0,0 +1,46 @@
+<div id="EditAccountModal" class="modal fade" role="dialog" data-backdrop="static" data-keyboard="false">
+  <div class="modal-dialog modal-md">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title"><i class="fa fa-pencil-square-o"></i> Edit Account</h4>
+      </div>
+      <form onsubmit="window.location.reload()" method="post" action="" id="editUserModalForm">
+        <div class="modal-body">
+          <div class="row">
+              <div class="col-md-12 form-group">
+                <label class="col-md-3">ID:</label>
+                <div class="col-md-9"><input type="text" class="form-control input-sm" name="accountID" readonly="readonly"/></div>
+              </div>
+              <div class="col-md-12 form-group">
+                <label class="col-md-3">Username:</label>
+                <div class="col-md-9"><input type="number" class="form-control input-sm" name="editUserUsername"/></div>
+              </div>
+              <div class="col-md-12 form-group">
+                <label class="col-md-3">ID Number:</label>
+                <div class="col-md-9"><input type="text" class="form-control input-sm" name="editUserIdNumber"/></div>
+              </div>
+              <div class="col-md-12 form-group">
+                <label class="col-md-3">Firstname:</label>
+                <div class="col-md-9"><input type="text" class="form-control input-sm" name="editUserFirstname"/></div>
+              </div>
+              <div class="col-md-12 form-group">
+                <label class="col-md-3">Middlename:</label>
+                <div class="col-md-9"><input type="text" class="form-control input-sm" name="editUserMiddlename"/></div>
+              </div>
+              <div class="col-md-12 form-group">
+                <label class="col-md-3">Lastname:</label>
+                <div class="col-md-9"><input type="text" class="form-control input-sm" name="editUserLastname"/></div>
+              </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-success">Edit</button>
+          <button type="button" class="btn btn-defWault" data-dismiss="modal">Cancel</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
